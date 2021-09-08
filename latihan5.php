51dd0baaf1fe7a731a00839a24ea0e4425a65721
Rahmatul647 <rahmatul_azizah_29rpl@student.smktelkom-mlg.sch.id>
2021-09-08 08:57:25 +0700

    commit ketiga

---
+++ latihan5.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Variabel antar Halaman</title>
+</head>
+<body>
+    <form action = "variable_hasil.php" method="post">
+        nama :<input type="text" name="nama"><br>
+        email : <input type = "text" name = "email"><br>
+        <input type = "submit" name ="submit" value = "Tampilkan">
+</form>
+</body>
+</html>    
